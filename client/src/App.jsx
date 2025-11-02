80a402d2630adb836c80209bcd5a12ff5d11d5b9
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2025-11-02 07:50:18 +0600

    footer page degain complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
-
+const router = createBrowserRouter(
+    createRoutesFromElements(
+     <Route>
+        <Route element ={<Main/>}>
+          <Route path="/" element = {<Home/>}/>
+          <Route path="/shop" element = {<Shop/>}/>
+          <Route path="/contact" element = {<Contact/>}/>
+          <Route path="/singleproduct" element = {<Singleproduct/>}/>
+          <Route path="/cart" element = {<Cart/>}/>
+          <Route path="/verified/:email" element = {<Verified/>}/>
+        </Route>
+     </Route>
+    )
+  );
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider 
+      router={router}
+    />
   )
 }
 
---
+++ client/src/layOut/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { RiAccountCircleLine } from "react-icons/ri";
+import { IoSearchOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { BsCart3 } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <nav className='w-full pt-[23px] pb-9 px-0 bg-white'>
+        <div className='container'>
+            <div className='flex items-center justify-between flex-wrap'>
+                <div className=' w-[205px] h-[45px] overflow-hidden'>
+                    <img className='w-full h-full object-cover' src={logo} alt='not found' />
+                </div>
+                <div className='flex items-center justify-center'>
+                    <ul className='flex items-center justify-center gap-x-[75px]'>
+                        <li className='text-base font-medium text-black capitalize cursor-pointer'>
+                            <Link to ='/'>home</Link>
+                        </li>
+                        <li className='text-base font-medium text-black capitalize cursor-pointer'>
+                            <Link to ='shop'>shop</Link>
+                        </li>
+                        <li className='text-base font-medium text-black capitalize cursor-pointer'>
+                            <Link to ='about'>about</Link>
+                        </li>
+                        <li className='text-base font-medium text-black capitalize cursor-pointer'>
+                            <Link to ='contact'>contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex items-center justify-center gap-x-[45px]'>
+                    <Link className='text-[28px] text-black cursor-pointer flex items-center justify-cente' to = "userprofile">
+                        <RiAccountCircleLine />
+                    </Link>
+                    <Link className='text-[28px] text-black cursor-pointer flex items-center justify-cente' to = "search">
+                        <IoSearchOutline />
+                    </Link>
+                    <Link className='text-[28px] text-black cursor-pointer flex items-center justify-cente' to ="heart">
+                        <FaRegHeart />
+                    </Link>
+                    <Link className='text-[28px] text-black cursor-pointer flex items-center justify-cente' to = "cart">
+                        <BsCart3 />
+                    </Link>
+                    <button className='bg-[#418b16] text-lg font-semibold text-white cursor-pointer capitalize px-5 py-2.5 rounded-[10px]'>
+                        login
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
